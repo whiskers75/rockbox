4f7a91bd5ac164d7bdfd3316b79ff477b1d6a352
whiskers75 <whiskers75@me.com>
2012-12-27 18:36:46 +0000

    An end to Travis suffering. Now, back to work (please?)
    Change-Id: Ieeaa1257e875b8a85215299121598588a86a255e

--- tools/rockboxdevgcc.sh
+++ tools/rockboxdevgcc.sh
@@ -202,28 +202,6 @@ build() {
 
     echo "ROCKBOXDEV: cd build-$toolname"
     cd build-$toolname
-
-    echo "ROCKBOXDEV: $toolname/configure"
-    case $toolname in
-        ctng) # ct-ng doesnt support out-of-tree build and the src folder is named differently
-            toolname="crosstool-ng"
-            cp -r ../$toolname-$version/* ../$toolname-$version/.version .
-            ./configure --prefix=$prefix $configure_params
-        ;;
-        *)
-            CFLAGS=-U_FORTIFY_SOURCE ../$toolname-$version/configure --target=$target --prefix=$prefix --enable-languages=c --disable-libssp --disable-docs $configure_params
-        ;;
-    esac
-
-    echo "ROCKBOXDEV: $toolname/make"
-    $make
-
-    echo "ROCKBOXDEV: $toolname/make install"
-    $make install
-
-    echo "ROCKBOXDEV: rm -rf build-$toolname $toolname-$version"
-    cd ..
-    rm -rf build-$toolname $toolname-$version
 }
 
 
