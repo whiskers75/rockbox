f65fddacdbabead7b37e0578acc3b1ab5fa09bd3
whiskers75 <whiskers75@me.com>
2012-12-31 15:00:46 +0000

    Boomshine total score doesnt work on a clip+
    Change-Id: Ide76cb5e6e5dc1c31b7a48f357a110b1eb5fa039

--- apps/plugins/boomshine.lua
+++ apps/plugins/boomshine.lua
@@ -283,7 +283,7 @@ function start_round(level, goal, nrBalls, total)
         draw_positioned_string(0, 0, string.format("%d balls expanded", nrExpandedBalls))
         draw_positioned_string(0, 1, string.format("Level %d", level))
         draw_positioned_string(1, 1, string.format("%d level points", score))
-        draw_positioned_string(1, 0, string.format("%d total points", total+score))
+        -- draw_positioned_string(1, 0, string.format("%d total points", total+score)) Does not work on clip+
 
         for _, ball in ipairs(balls) do
             ball:step()
