7cbef10d9eaf0e767fe2ea566339f63a0c9dfb66
whiskers75 <crazycats@yopmail.com>
2013-01-27 03:43:15 -0500

    SUDO SCREWED IT UP

--- travis/configstage.sh
+++ travis/configstage.sh
@@ -2,7 +2,7 @@ while sleep 5
 do printf .
 done & 
 export x=$! 
-sudo ../travis/configure.sh > /dev/null 2>/dev/null &
+../travis/configure.sh > /dev/null 2>/dev/null &
 wait "$!" 
 kill "$x"
 echo "COMPLETED"
